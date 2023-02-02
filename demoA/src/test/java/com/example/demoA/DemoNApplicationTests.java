f8c396de014606b66bdbc1e9f3a13b167f279ebc
CodeSandbox <support@codesandbox.io>
2023-02-02 16:59:27 +0000

    Initial commit

---
+++ demoA/src/test/java/com/example/demoA/DemoNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
